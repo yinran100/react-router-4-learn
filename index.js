ae2876ff6b78539ac53731dc5ef4ce732dcce0e6
yinran100 <1005469156@qq.com>
2019-12-29 17:24:52 +0800

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import StaticRouter from './static-router.js'; // 静态路由
+import DynamicRouter from './dynamic-router.js'; // 动态路由
 import './style.css';
 
-// class App extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       name: 'React'
-//     };
-//   }
+// render(<StaticRouter />, document.getElementById('root')); // 静态路由
 
-//   render() {
-//     return (
-//       <div>
-//         <Hello name={this.state.name} />
-//         <p>
-//           Start editing to see some magic happen :)
-//         </p>
-//       </div>
-//     );
-//   }
-// }
+render(<DynamicRouter />, document.getElementById('root'));
 
-render(<StaticRouter />, document.getElementById('root'));
--- static-router.js
+++ static-router.js
@@ -1,12 +1,16 @@
 // V3 静态路由
 import React from 'react';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, browserHistory, Link } from 'react-router'
 
 const PrimaryLayout = props => (
   <div className="primary-layout">
     <header>
       Our React Router 3 App
     </header>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/users">Users</Link></li>
+    </ul>
     <main>
       {props.children}
     </main>
